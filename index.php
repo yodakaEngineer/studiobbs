bfc3eaaa1a7c7ca49d10d68c1827fee743d3fe67
yodaka <love.bande.gtv@gmail.com>
2018-12-02 14:44:52 +0900

    design change

--- index.php
+++ index.php
@@ -1,10 +1,21 @@
 <?php require_once 'header.php'; ?>
 <main>
-  <p><a href="reserve.php">練習室を予約</a></p>
-  <form class="" action="index.php" method="post">
-    <input type="text" name="comment" value="" placeholder="コメント">
-    <input type="submit" value="送信">
-  </form>
+  <p class="toReserve"><a href="reserve.php">練習室を予約する</a></p>
+  <div class="Desc">
+    <h2>練習室用予約フォームの使い方</h2>
+    <div class="">
+      <p>１、カレンダーの予約したい、または予約の確認をしたい日付を選択</p>
+      <p>２、カレンダーの下にその日の予約状況とフォームが出ます</p>
+      <p>３、予約する場合はフォームに必要事項を記入して送信</p>
+      <p>４、ページが更新されたら予約した日付を選択して予約が反映されているか確認してみてね</p>
+      <div class="">
+        <h3>～予約を取り消したい場合～</h3>
+        <p>１、予約状況の取り消したい予約を選択</p>
+        <p>２、削除するボタンを押す</p>
+      </div>
+      <p>何かあればenginneryodaka@gmail.comまでお知らせください。</p>
+    </div>
+  </div>
 </main>
 <footer>
 </footer>
